652ba1195bb78bca61d5add3636172957bc517c5
hnrqss <hnrq.assuncao@gmail.com>
2018-08-17 20:13:35 -0300

    import bootstrap 4

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import React, { Component } from 'react'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div>
+          
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
+// bootstrap 4
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
