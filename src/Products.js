4cfab82c7366111b8ec9d7c24e25436063c6d7ca
hnrqss <hnrq.assuncao@gmail.com>
2018-08-18 18:35:11 -0300

    get params from url to categories

--- src/Products.js
+++ src/Products.js
@@ -1,9 +1,28 @@
 import React, {Component} from 'react'
+import { Route, Link } from 'react-router-dom'
+
+import ProductsHome from './ProductsHome'
+import Category from './Category'
 
 export default class Products extends Component {
     render() {
+        const { match } = this.props
         return(
-            <h1>Products</h1>
+            <div className='row'>
+                
+                <div className='col-md-2'>
+                    
+                    <h3>Categories</h3>
+                    <Link to='/products/category/1'>Category 1</Link>
+                </div>
+                
+                <div className='col-md-10'>
+                    
+                    <h3>Products</h3>
+                    <Route exact path={match.url} component={ProductsHome} />
+                    <Route exact path={match.url+'/category/:catId'} component={Category} />
+                </div>
+            </div>
         )
     }
 }
